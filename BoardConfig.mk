4ac86580f65c65378c6f8dee2e9d223a814b210a
Jenkins <jenkins@psoucywork.(none)>
2013-12-31 10:32:07 -0500

    initial drop from phytec

---
+++ BoardConfig.mk
@@ -0,0 +1,108 @@
+#
+# Copyright (C) 2011 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# define OMAP_ENHANCEMENT variables
+include device/phytec/pcm049/Config.mk
+
+# These two variables are set first, so they can be overridden
+# by BoardConfigVendor.mk
+BOARD_USES_GENERIC_AUDIO := false
+USE_CAMERA_STUB := true
+
+OMAP_ENHANCEMENT := true
+OMAP_ENHANCEMENT_MULTIGPU := true
+
+# Use the non-open-source parts, if they're present
+#-include vendor/phytec/pcm049/BoardConfigVendor.mk
+
+ENHANCED_DOMX := true
+BLTSVILLE_ENHANCEMENT := true
+BOARD_USES_DVP := true
+BOARD_USES_ARX := true
+#USE_ITTIAM_AAC := true
+
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+BOARD_HAVE_BLUETOOTH := false
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RECOVERY := true
+TARGET_NO_KERNEL := true
+
+BOARD_KERNEL_BASE := 0x80000000
+#BOARD_KERNEL_CMDLINE :=
+
+TARGET_NO_RADIOIMAGE := true
+TARGET_BOARD_PLATFORM := omap4
+TARGET_BOOTLOADER_BOARD_NAME := pcm049 
+
+BOARD_EGL_CFG := device/phytec/pcm049/egl.cfg
+
+#BOARD_USES_HGL := true
+#BOARD_USES_OVERLAY := true
+USE_OPENGL_RENDERER := true
+
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 268435456
+#BOARD_SYSTEMIMAGE_PARTITION_SIZE := 16777216
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 536870912
+BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_FLASH_BLOCK_SIZE := 4096
+
+USES_TI_MAC80211 := true
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wl12xx
+BOARD_WLAN_DEVICE                := wl12xx_mac80211
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_wl12xx
+BOARD_SOFTAP_DEVICE              := wl12xx_mac80211
+WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wl12xx_sdio.ko"
+WIFI_DRIVER_MODULE_NAME          := "wl12xx_sdio"
+WIFI_FIRMWARE_LOADER             := ""
+COMMON_GLOBAL_CFLAGS += -DUSES_TI_MAC80211
+
+
+ifdef OMAP_ENHANCEMENT
+COMMON_GLOBAL_CFLAGS += -DOMAP_ENHANCEMENT -DTARGET_OMAP4
+ifdef USE_ITTIAM_AAC
+COMMON_GLOBAL_CFLAGS += -DUSE_ITTIAM_AAC
+endif
+ifdef OMAP_ENHANCEMENT_S3D
+COMMON_GLOBAL_CFLAGS += -DOMAP_ENHANCEMENT_S3D
+endif
+ifdef OMAP_ENHANCEMENT_CPCAM
+COMMON_GLOBAL_CFLAGS += -DOMAP_ENHANCEMENT_CPCAM
+endif
+ifdef OMAP_ENHANCEMENT_VTC
+COMMON_GLOBAL_CFLAGS += -DOMAP_ENHANCEMENT_VTC
+endif
+endif
+
+#Set 32 byte cache line to true
+ARCH_ARM_HAVE_32_BYTE_CACHE_LINES := true
+
+
+#TARGET_PROVIDES_INIT_RC := true
+#TARGET_USERIMAGES_SPARSE_EXT_DISABLED := true
+
+# Common device independent definitions
+include device/ti/common-open/BoardConfig.mk
---
+++ device.mk
@@ -0,0 +1,158 @@
+#
+# Copyright (C) 2011 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# define OMAP_ENHANCEMENT variables
+include device/phytec/pcm049/Config.mk
+
+PRODUCT_COPY_FILES := \
+	device/phytec/pcm049/init.pcm049.rc:root/init.pcm049.rc \
+	device/phytec/pcm049/init.pcm049.usb.rc:root/init.pcm049.usb.rc \
+	device/phytec/pcm049/init.pcm049.wifi.rc:root/init.pcm049.wifi.rc \
+        device/phytec/pcm049/fstab.pcm049:root/fstab.pcm049 \
+	device/phytec/pcm049/ueventd.pcm049.rc:root/ueventd.pcm049.rc \
+	device/phytec/pcm049/mixer_paths.xml:system/etc/mixer_paths.xml \
+	device/phytec/pcm049/media_profiles.xml:system/etc/media_profiles.xml \
+	device/phytec/pcm049/media_codecs.xml:system/etc/media_codecs.xml \
+	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+	frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+	frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+	device/phytec/pcm049/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
+	device/phytec/pcm049/omap4-keypad.kl:system/usr/keylayout/omap4-keypad.kl \
+	device/phytec/pcm049/omap4-keypad.kcm:system/usr/keychars/omap4-keypad.kcm \
+        device/phytec/pcm049/ft5x06_ts.idc:system/usr/idc/ft5x06_ts.idc \
+        #device/phytec/pcm049/twl6030_pwrbutton.kl:system/usr/keylayout/twl6030_pwrbutton.kl \
+	#device/phytec/pcm049/audio/audio_policy.conf:system/etc/audio_policy.conf \
+
+# to mount the external storage (sdcard)
+PRODUCT_COPY_FILES += \
+        device/phytec/pcm049/vold.fstab:system/etc/vold.fstab
+
+# Live Wallpapers
+PRODUCT_PACKAGES += \
+        LiveWallpapers \
+        LiveWallpapersPicker \
+        MagicSmokeWallpapers \
+        VisualizationWallpapers \
+        librs_jni
+
+PRODUCT_PACKAGES += \
+    ti_omap4_ducati_bins
+#    libOMX_Core \
+#    libOMX.TI.DUCATI1.VIDEO.DECODER
+
+# Tiler
+PRODUCT_PACKAGES += \
+    libtimemmgr
+
+# Camera
+ifdef OMAP_ENHANCEMENT_CPCAM
+PRODUCT_PACKAGES += \
+    libcpcam_jni \
+    com.ti.omap.android.cpcam
+
+PRODUCT_COPY_FILES += \
+    hardware/ti/omap4xxx/cpcam/com.ti.omap.android.cpcam.xml:system/etc/permissions/com.ti.omap.android.cpcam.xml
+endif
+
+PRODUCT_PACKAGES += \
+    CameraOMAP \
+    Camera \
+    camera_test
+
+PRODUCT_PROPERTY_OVERRIDES := \
+        hwui.render_dirty_regions=false
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+        persist.sys.usb.config=mtp
+
+PRODUCT_PROPERTY_OVERRIDES += \
+        ro.opengles.version=131072
+
+PRODUCT_PROPERTY_OVERRIDES += \
+        ro.sf.lcd_density=240
+
+PRODUCT_PACKAGES += \
+        make_ext4fs \
+	com.android.future.usb.accessory
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	wifi.interface=wlan0 \
+	hwui.render_dirty_regions=false
+
+PRODUCT_CHARACTERISTICS := tablet,nosdcard
+
+DEVICE_PACKAGE_OVERLAYS := \
+    device/phytec/pcm049/overlay
+
+#HWC Hal
+PRODUCT_PACKAGES += \
+    hwcomposer.omap4
+
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+PRODUCT_PACKAGES += \
+	librs_jni \
+	com.android.future.usb.accessory
+
+#PRODUCT_PACKAGES += \
+	audio.primary.pcm049
+
+# tinyalsa utils
+#PRODUCT_PACKAGES += \
+	tinymix \
+	tinyplay \
+	tinycap
+
+# Audioout libs
+#PRODUCT_PACKAGES += libaudioutils
+
+# Enable AAC 5.1 decode (decoder)
+PRODUCT_PROPERTY_OVERRIDES += \
+	media.aac_51_output_enabled=true
+
+PRODUCT_PACKAGES += \
+	dhcpcd.conf \
+	TQS_D_1.7.ini \
+	TQS_D_1.7_127x.ini \
+	hostapd.conf \
+	wifical.sh \
+	calibrator
+
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+	make_ext4fs
+
+# BlueZ test tools
+#PRODUCT_PACKAGES += \
+#	hciconfig \
+#	hcitool
+
+DUCATI_TGZ := device/ti/proprietary-open/omap4/ducati_blaze_tablet.tgz
+PRODUCT_PACKAGES += ducati-m3-core0.xem3
+
+$(call inherit-product-if-exists, vendor/phytec/pcm049/device-vendor.mk)
+$(call inherit-product, frameworks/native/build/tablet-dalvik-heap.mk)
+$(call inherit-product, hardware/ti/omap4xxx/omap4.mk)
+$(call inherit-product-if-exists, device/ti/proprietary-open/omap4/ti-omap4-vendor.mk)
+$(call inherit-product-if-exists, vendor/ti/proprietary/omap4/ti-omap4-vendor.mk)
+$(call inherit-product, device/phytec/pcm049/wl12xx/ti-wl12xx-vendor.mk)
+$(call inherit-product-if-exists, device/ti/proprietary-open/wl12xx/wlan/wl12xx-wlan-fw-products.mk)
+#$(call inherit-product-if-exists, device/ti/common-open/s3d/s3d-products.mk)
+#$(call inherit-product-if-exists, device/ti/proprietary-open/omap4/ducati-full_blaze.mk)
+#$(call inherit-product-if-exists, device/ti/proprietary-open/omap4/dsp_fw.mk)
+$(call inherit-product-if-exists, hardware/ti/dvp/dvp-products.mk)
+$(call inherit-product-if-exists, hardware/ti/arx/arx-products.mk)
